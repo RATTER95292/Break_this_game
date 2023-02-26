401e3498c1bbde75af9e9758f6a1bd85fe768280
Monte-Kristo <83087001+RATTER95292@users.noreply.github.com>
2023-02-26 23:06:34 +0300

    Update Karate_Kido.py

--- Karate_Kido.py
+++ Karate_Kido.py
@@ -1,1 +1,32 @@
+'''
+2 имитирование работы с помощью стрелочек
+3 обрезание фото
+4 перевод изображение в другой формат
+5 распознование опасности
+6 распозноваие расположения каратиста
+
+'''
+# Program for partial screenshot
+
+import pyautogui
+import time
+import keyboard
+
+t = False
+while True:
+
+    print(keyboard.read_key())
+    if keyboard.read_key() == "right shift":
+        t = True
+
+    while t:
+        time.sleep(3)
+        screen = pyautogui.screenshot('screenshot.png',region=(655,211, 607, 808))
+        if keyboard.read_key() == "shift":
+            t = False
+    if keyboard.read_key() == "ctrl":
+        break
+
+
+
 
